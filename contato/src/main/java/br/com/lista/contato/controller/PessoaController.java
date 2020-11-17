07a754c882cd8f893b9ebc0893819fbb1b7c1ce3
Gabriel <Gabriel@192.168.25.7>
2020-11-16 21:03:37 -0300

    alteração e criação de duas classes Controller para melhor organização

--- contato/src/main/java/br/com/lista/contato/controller/PessoaController.java
+++ contato/src/main/java/br/com/lista/contato/controller/PessoaController.java
@@ -13,12 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.lista.contato.model.Pessoa;
-import br.com.lista.contato.model.Telefone;
 import br.com.lista.contato.repository.PessoaRepository;
 import br.com.lista.contato.repository.TelefoneRepository;
 
 @Controller
-public class ContatoController {
+public class PessoaController {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
@@ -121,51 +120,6 @@ public class ContatoController {
 
 	}
 
-	@PostMapping("**/salvarfone/{pessoaid}")
-	public ModelAndView salvarTelefone(@PathVariable("pessoaid") Long pessoaid, Telefone telefone) {
-		Pessoa pessoa = pessoaRepository.findById(pessoaid).get();
-		
-		if (telefone != null && telefone.getNumero().isEmpty() || telefone.getTipo().isEmpty()) {
-			
-			ModelAndView modelAndView = new ModelAndView("/cadastro/telefones");
-			modelAndView.addObject("objpessoa", pessoa);
-			modelAndView.addObject("telefones", telefoneRepository.listaTelefone(pessoaid));
-			
-			List<String> msg = new ArrayList<String>();
-			if(telefone.getNumero().isEmpty()) {
-				msg.add("Digite um número");
-			}
-			
-			if(telefone.getTipo().isEmpty()) {
-				msg.add("Digite um Tipo");
-			}
-			
-			modelAndView.addObject("msg", msg);
-			
-			return modelAndView;
-		} 
-		telefone.setPessoa(pessoa);
-		telefoneRepository.save(telefone);
-		ModelAndView modelAndView = new ModelAndView("/cadastro/telefones");
-		modelAndView.addObject("objpessoa", pessoa);
-		modelAndView.addObject("telefones", telefoneRepository.listaTelefone(pessoaid));
-		
-		return modelAndView;
-		
-	}
 	
-	@GetMapping("/excluirfone/{idfone}")
-	public ModelAndView excluirFone(@PathVariable("idfone") Long idfone) {
-		Pessoa pessoa = telefoneRepository.findById(idfone).get().getPessoa();
-
-		telefoneRepository.deleteById(idfone);
-
-		ModelAndView modelAndView = new ModelAndView("/cadastro/telefones");
-		modelAndView.addObject("objpessoa", pessoa);
-		modelAndView.addObject("telefones", telefoneRepository.listaTelefone(pessoa.getId()));
-
-		return modelAndView;
-
-	}
 
 }
